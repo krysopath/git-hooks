6f7724a19d5badbd367eaf55a72ac34000761d9e
Georg vom Endt <gve@3yourmind.com>
2020-01-17 22:54:00 +0100

    hooks(add-shfmt-autoformatter):add slightly hook

--- hooks/pre-commit.d/00-check-filenames-and-whitespaces.sh
+++ hooks/pre-commit.d/00-check-filenames-and-whitespaces.sh
@@ -11,10 +11,9 @@ allownonascii="$(git config --bool hooks.allownonascii)"
 
 # Redirect output to stderr.
 exec 1>&2
-
-if [ "$allownonascii" != "true" ] &&
-    test $(git diff --cached --name-only --diff-filter=A -z "$against" |
-        LC_ALL=C tr -d '[ -~]\0' | wc -c) != 0; then
+if [ "$allownonascii" != "true" ] \
+    && test $(git diff --cached --name-only --diff-filter=A -z "$against" \
+        | LC_ALL=C tr -d '[ -~]\0' | wc -c) != 0; then
     cat <<\EOF
 Error: Attempt to add a non-ASCII file name.
 
@@ -26,7 +25,6 @@ doing you can disable this check using:
 EOF
     exit 1
 fi
-
 # If there are whitespace errors, print the offending file names and fail.
 # shellcheck disable=SC2093
 exec git diff-index --check --cached "$against" --
--- hooks/pre-commit.d/11-shfmt-shellscripts.sh
+++ hooks/pre-commit.d/11-shfmt-shellscripts.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+STAGED="$(git diff --staged --name-only)"
+CHANGED="$(grep '^.*\.sh' <<<"$STAGED" || [[ $? == 1 ]])"
+LINT_SHELL=bash
 
-echo $(which shfmt)
+SHFMT_FORCE=true
+SHFMT="$(which shfmt)"
+SHFMT_ARGS="-ln $LINT_SHELL -ci -bn -i 4 -w"
+
+set -x
+
+if [ ! -z "$SHFMT" ]; then
+    if [ ! -e "$CHANGED" ]; then
+        # run shfmt with args
+        "$SHFMT" $SHFMT_ARGS $CHANGED
+        # Stage updated/shfmt'ed files
+        git add -u
+    fi
+else
+    echo "$(tput setaf 4)WARN: $0: no shfmt found in PATH: $PATH"
+    echo -e "Run\n\tGO111MODULE=on go get github.com/mvdan/sh"
+    if [ "$SHFMT_FORCE" == "true" ]; then
+        echo "$(tput setaf 1)ERROR: $0: no shfmt binary in PATH"
+        exit 1
+    fi
+fi
