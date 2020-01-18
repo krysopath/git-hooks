dbea67ad3c9e6ec53882a71b79e4e4886659b965
Georg vom Endt <gve@3yourmind.com>
2020-01-18 23:22:49 +0100

    hooks(* add-shfmt-autoformatter):fix added files

--- hooks/hook.sh
+++ hooks/hook.sh
@@ -22,18 +22,18 @@
 #                 HMS HOOK
 
 echo "$(
-    tput bold
-    tput setaf 1
-)running $(basename "$0")"
+	tput bold
+	tput setaf 1
+)+ running $(basename "$0")"
 
 for h in "$(dirname "$0")/$(basename "$0")".d/*; do
-    if [ -x "$h" ]; then
-        echo "$(
-            tput bold
-            tput setaf 2
-        )running $h"
-        $h || exit 1
-    else
-        echo "$h was marked with '-x' to not execute"
-    fi
+	if [ -x "$h" ]; then
+		echo "$(
+			tput bold
+			tput setaf 2
+		)+ running $h"
+		$h || exit 1
+	else
+		echo "- $h is not non-executable"
+	fi
 done
--- hooks/pre-commit.d/11-shfmt-shellscripts.sh
+++ hooks/pre-commit.d/11-shfmt-shellscripts.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
+set -x
+
 STAGED="$(git diff --staged --name-only)"
 CHANGED="$(grep '^.*\.sh' <<<"$STAGED" || [[ $? == 1 ]])"
 LINT_SHELL=bash
 
 SHFMT_FORCE=true
 SHFMT="$(which shfmt)"
-SHFMT_ARGS="-ln $LINT_SHELL -ci -bn -i 4 -w"
+SHFMT_ARGS="-ln $LINT_SHELL -ci -bn -w"
 
 if [ ! -z "$SHFMT" ]; then
-    if [ ! -e "$CHANGED" ]; then
-        # run shfmt with args
-        "$SHFMT" $SHFMT_ARGS $CHANGED
-        # Stage updated/shfmt'ed files
-        git add -u
-    fi
+	if [ ! -z "$CHANGED" ]; then
+		# run shfmt with args
+		# shellcheck disable=SC2086
+		$SHFMT $SHFMT_ARGS $CHANGED
+		# Stage updated/shfmt'ed files
+		#git add "$CHANGED"
+	fi
 else
-    echo "$(tput setaf 4)WARN: $0: no shfmt found in PATH: $PATH"
-    echo -e "Run\n\tGO111MODULE=on go get github.com/mvdan/sh"
-    if [ "$SHFMT_FORCE" == "true" ]; then
-        echo "$(tput setaf 1)ERROR: $0: no shfmt binary in PATH"
-        exit 1
-    fi
+	echo "$(tput setaf 4)WARN: $0: no shfmt found in PATH: $PATH"
+	echo -e "Run\n\tGO111MODULE=on go get github.com/mvdan/sh"
+	if [ "$SHFMT_FORCE" == "true" ]; then
+		echo "$(tput setaf 1)ERROR: $0: no shfmt binary in PATH"
+		exit 1
+	fi
 fi
